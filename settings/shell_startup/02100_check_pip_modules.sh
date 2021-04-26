913cf16dc230b9bcf666255a2efb21cefe69ba9e
Jeff Hykin <jeff.hykin@gmail.com>
2021-04-25 21:38:56 -0500

    reorganize everything

--- settings/shell_startup/02100_check_pip_modules.sh
+++ settings/shell_startup/02100_check_pip_modules.sh
@@ -1,5 +1,6 @@
 "$PROJECTR_FOLDER/settings/commands/.check_pip_modules"
 
 # special local packages
-python -m pip --disable-pip-version-check install -e ./ml-agents-envs
-python -m pip --disable-pip-version-check install -e ./ml-agents
+python -m pip --disable-pip-version-check install -e ./source/modules/ml-agents-envs
+python -m pip --disable-pip-version-check install -e ./source/modules/ml-agents
+python -m pip --disable-pip-version-check install -e ./source/modules/gym-unity
