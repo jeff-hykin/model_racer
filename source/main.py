2a52610ae9d74d22542dabcca258dbb9f07afa1d
Jeff Hykin <jeff.hykin@gmail.com>
2021-05-02 18:41:37 -0500

    remove incorrect comment

--- source/main.py
+++ source/main.py
@@ -38,7 +38,6 @@ try:
             actions = env.action_space.sample()  # rotation, acceleration
             actions = np.array([0,1])
             obs, reward, done, _ = env.step(actions)
-            # note: returns a generator, not a list
             episode_rewards += reward
         print(f"Total reward this episode: {episode_rewards}")
 finally:
