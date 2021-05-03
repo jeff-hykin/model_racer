b317d5982f5d2bad33445104b4747def0419d788
Jeff Hykin <jeff.hykin@gmail.com>
2021-05-03 10:06:01 -0500

    formatting

--- vae/controller.py
+++ vae/controller.py
@@ -21,9 +21,16 @@ class VAEController:
     :param batch_size: (int)
     :param normalization_mode: (str)
     """
-    def __init__(self, z_size=None, input_dimension=(80, 112, 3),
-                 learning_rate=0.0001, kl_tolerance=0.5,
-                 batch_size=64, normalization_mode='rl'):
+
+    def __init__(
+        self,
+        z_size=None,
+        input_dimension=(80, 112, 3),
+        learning_rate=0.0001,
+        kl_tolerance=0.5,
+        batch_size=64,
+        normalization_mode="rl",
+    ):
         # VAE input and output shapes
         self.z_size = z_size
         self.input_dimension = input_dimension
@@ -40,37 +47,43 @@ class VAEController:
         self.target_vae = None
 
         if z_size is not None:
-            self.vae = ConvVAE(z_size=self.z_size,
-                               batch_size=self.batch_size,
-                               learning_rate=self.learning_rate,
-                               kl_tolerance=self.kl_tolerance,
-                               is_training=True,
-                               reuse=False)
+            self.vae = ConvVAE(
+                z_size=self.z_size,
+                batch_size=self.batch_size,
+                learning_rate=self.learning_rate,
+                kl_tolerance=self.kl_tolerance,
+                is_training=True,
+                reuse=False,
+            )
 
-            self.target_vae = ConvVAE(z_size=self.z_size,
-                                      batch_size=1,
-                                      is_training=False,
-                                      reuse=False)
+            self.target_vae = ConvVAE(
+                z_size=self.z_size, batch_size=1, is_training=False, reuse=False
+            )
 
     def encode_from_raw_image(self, raw_image):
         """
         :param raw_image: (np.ndarray) BGR image
         """
         r = ROI
-        print('raw_image.shape = ', raw_image.shape)
+        print("raw_image.shape = ", raw_image.shape)
         # Crop image
-        im = raw_image[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
+        im = raw_image[int(r[1]) : int(r[1] + r[3]), int(r[0]) : int(r[0] + r[2])]
         return self.encode(im)
 
     def encode(self, observation):
-        assert observation.shape == self.input_dimension, "{} != {}".format(observation.shape, self.input_dimension)
+        assert observation.shape == self.input_dimension, "{} != {}".format(
+            observation.shape, self.input_dimension
+        )
         # Normalize
-        observation = preprocess_input(observation.astype(np.float32),
-                                       mode=self.normalization_mode)[None]
+        observation = preprocess_input(
+            observation.astype(np.float32), mode=self.normalization_mode
+        )[None]
         return self.target_vae.encode(observation)
 
     def decode(self, arr):
-        assert arr.shape == (1, self.z_size), "{} != {}".format(arr.shape, (1, self.z_size))
+        assert arr.shape == (1, self.z_size), "{} != {}".format(
+            arr.shape, (1, self.z_size)
+        )
         # Decode
         arr = self.target_vae.decode(arr)
         # Denormalize
